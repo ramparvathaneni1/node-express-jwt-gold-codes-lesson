6bdcb50fd457a9278d8bc35d2e3fb56a2d5e9533
ben <ben@localhost>
2023-12-20 11:24:01 -0500

    remove solution folder

--- solution/models/auth.js
+++
@@ -1,8 +0,0 @@
-class Auth {
-  static validate(username, password) {
-    // We could validate the userName/password from a database instead
-    return (username === 'admin' && password === 'securepassword');
-  }
-}
-
-module.exports = Auth;
